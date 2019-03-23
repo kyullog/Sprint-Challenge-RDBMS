6225c4b0428b9265bfb445b05d00fa6c4841f067
Jonathan Dorety <kyullog@gmail.com>
2019-03-23 13:12:44 -0400

    Refactor project router to remove useless code

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -4,18 +4,16 @@ const db = require("../data/helpers/projectHelpers.js");
 
 router.post("/", async (req, res) => {
   try {
-    if (!req.body) {
-      res.status(400).json({ error: "Please input a project to add" });
+    const project = req.body;
+    if (!project.project_name) {
+      res.status(400).json({ error: "A name is required for your project" });
     } else {
-      const project = req.body;
-      if (!project.project_name) {
-        res.status(400).json({ error: "A name is required for your project" });
-      } else {
-        const added = await db.insert(project);
-        res.status(201).json(project);
-      }
+      const added = await db.insert(project);
+      res.status(201).json(added);
     }
-  } catch (err) {}
+  } catch (err) {
+    res.status(500).json({ error: "There was a problem adding the project" });
+  }
 });
 
 module.exports = router;
