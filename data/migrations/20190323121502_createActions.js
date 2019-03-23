4a11adda20164030d58ec93ed3a1343cd7454085
Jonathan Dorety <kyullog@gmail.com>
2019-03-23 12:27:10 -0400

    Configure Actions migration

--- data/migrations/20190323121502_createActions.js
+++ data/migrations/20190323121502_createActions.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", tbl => {
+    tbl.increments();
+    tbl.string("action_description").notNullable();
+    tbl.text("notes");
+    tbl.boolean("action_completed").defaultTo(false);
+    tbl
+      .foreign("project_id")
+      .references("projects.id")
+      .unsigned();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("actions");
 };
