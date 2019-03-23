6afafd4876d8195e636b99f848b9f16c33211559
Jonathan Dorety <kyullog@gmail.com>
2019-03-23 14:09:19 -0400

    Add get project by id and get actions by id
    Refactor export structure for no real reason

--- data/helpers/projectHelpers.js
+++ data/helpers/projectHelpers.js
@@ -1,7 +1,20 @@
 const db = require("../dbConfig.js");
 
 module.exports = {
-  insert: project => {
-    return db("projects").insert(project);
-  }
+  insert,
+  getActionsByProject,
+  getById
 };
+function insert(project) {
+  return db("projects").insert(project);
+}
+
+function getActionsByProject(id) {
+  return db("actions").where({ project_id: id });
+}
+
+function getById(id) {
+  return db("projects")
+    .where({ id })
+    .first();
+}
