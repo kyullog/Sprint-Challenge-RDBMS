c477dafb369609a16e89cf336b2a64b14cee2a2e
Jonathan Dorety <kyullog@gmail.com>
2019-03-23 14:34:05 -0400

    Add get actions helper function

--- data/helpers/actionHelpers.js
+++ data/helpers/actionHelpers.js
@@ -3,5 +3,16 @@ const db = require("../dbConfig.js");
 module.exports = {
   insert: action => {
     return db("actions").insert(action);
+  },
+
+  get: () => {
+    return db("actions")
+      .select(
+        "action_description as description",
+        "notes",
+        "action_completed as completed",
+        "projects.project_name as project"
+      )
+      .innerJoin("projects", "actions.project_id", "projects.id");
   }
 };
