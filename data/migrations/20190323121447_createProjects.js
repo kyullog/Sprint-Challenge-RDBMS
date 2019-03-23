f7bbbc7f3505e7862cd40eb6a5c59ff3b692cd0d
Jonathan Dorety <kyullog@gmail.com>
2019-03-23 12:21:14 -0400

    Configure Projects migration

--- data/migrations/20190323121447_createProjects.js
+++ data/migrations/20190323121447_createProjects.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("projects", tbl => {
+    tbl.increments();
+    tbl.string("project_name").notNullable();
+    tbl.text("project_description");
+    tbl.boolean("project_completed").defaultTo(false);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("projects");
 };
