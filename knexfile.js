05dd39db08882bb27a4cdc3bfd077d7f37969c4c
Jonathan Dorety <kyullog@gmail.com>
2019-03-26 21:11:22 -0400

    Comment out all but development config

--- knexfile.js
+++ knexfile.js
@@ -13,37 +13,38 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
-  },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
   }
+  // ,
+  //
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
+  //
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // }
 };
